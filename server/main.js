3f384200f457c01267a6951ebe23dffff6c3e12c
chuck <leejh10003@korea.ac.kr>
2019-06-04 20:12:33 +0900

    Translate into English

--- server/main.js
+++ server/main.js
@@ -84,23 +84,23 @@ Meteor.startup(() => {
                 }
                 ,contentTag:faker.fake([
 					{
-						"tag" : "여행"
+						"tag" : "Travel"
 					},
 					{
-						"tag" : "필리"
+						"tag" : "Philippines"
 					},
 					{
-						"tag" : "심해"
+						"tag" : "Deep Ocean"
 					}
 				])
                 ,contentName:faker.random.arrayElement([
-                    "보홀의 푸른 바닷속 생물들과 함께",
-                    "일본의 교토의 일상을 담는 카메라",
-                    "상공에서 보이는 하얀 구름세상",
-                    "신생아로 태어나 죽음까지, 다큐 이미지",
-                    "나이지리아의 원주민과 함께하는 자연의 삶",
-                    "세계의 여러 결혼식을 기록하다",
-                    "유럽 국가들의 클럽클럽클럽 흔들어"
+                    "Together with lives in blue sea of bohol",
+                    "Camera captures daily lives of Kyoto, Japan",
+                    "White coluds in the sky",
+                    "From cradle to grave, documentary images",
+                    "Nature lives with naigirian autochtones",
+                    "Marriages around the world recorded",
+                    "Feel the beeeatssss of european clubs"
                 ])
                 ,investedKlay:faker.random.number(1000)
                 ,investedStock:faker.random.number(1000)
@@ -112,37 +112,37 @@ Meteor.startup(() => {
                 ]
                 ,contentScore:faker.random.number(10)
                 ,contentDesc:faker.random.arrayElement([
-                    "보홀의 푸른 바닷속에 펼쳐지는 풍경 /\n/\r" +
-                    "그 속에 사는 수많은 생물들을 수중 카메라에 담기위해 떠나는 여행/<br>/g " +
-                    "그 어떤 사진과도 비교할 수 없는 작품을 만들것입니다.",
+                    "Incredible scenery under the blue sea of Bohol/\n/\r" +
+                    "Travel to capture sea full of lives/<br>/g " +
+                    "Will caputre masterpiece outstands to any other photographics",
 
-                    "일본의 교토의 일상을 담을 것입니다. " +
-                    "가깝지만 멀고도 먼 일본 교토... " +
-                    "조용하고 아늑하지만 그들만의 특색을 보존하고 있는 아름다운 도시" +
-                    "그 곳의 일상을 사진으로 담을 것입니다.",
+                    "Daily lives of Kyoto, Japan " +
+                    "Kyoto, so near and yet so far city" +
+                    "The charming city silent, cozy, and still keeping its own color." +
+                    "I will capture daily lives of there.",
 
-                    "상공에서 보이는 하얀 구름세상 " +
-                    "상공에서 특수 카메라와 함께 하늘에서 바라보는 하늘의 모습을 앵글에 담고" +
-                    "스카이 다이빙 전문가들의 다양한 묘기를 " +
-                    "한 순간의 이미지로 담아 올 것입니다.",
+                    "White coluds in the sky " +
+                    "I will capture images of special moments," +
+                    "Like scenery of sky captured upon the sky through special camera" +
+                    "And skydivings by experts",
 
-                    "신생아로 태어나 죽음에 이르기 까지 " +
-                    "일생을 다룬 다큐 이미지 모음 " +
-                    "그 누구도 시도 하지 않았던, 인간의 처음부터 끝까지의 시간을 " +
-                    "대담한 프로젝트의 일환으로 제작하고자 합니다.",
+                    "From cradle to grave " +
+                    "Set of documentary images about whole life" +
+                    "Unchallenged, capturing the start and end of a human's life" +
+                    "will be started, as part of bold project.",
 
-                    "나이지리아의 원주민과 함께하는 자연의 삶을 통해 만들 작품 " +
-                    "원주민들과의 3개월간 거친 동거를 통해 도시인들이 겪어볼수 없을 " +
-                    "태초의 경험들을 이미지로 담아올 것입니다. " +
-                    "해당 제품은 후진국 국민들을 돕는 필수품 지원을 위한 금액도 일부 포함되어 있습니다.",
+                    "Nature lives with naigirian autochtones " +
+                    "The experiences of premival can't expreienced through city life will be recorded as image, " +
+                    "through 3-months cohabit with autochtones " +
+                    "Price of this product contains donation for people of undeveloped countries' necessity.",
 
-                    "세계의 여러 결혼식을 기록하다 " +
-                    "우리는 대한민국이라는 결혼식에 대해서만 알고있습니다. " +
-                    "하지만 세상에는 수많은 결혼식이 저만의 방식으로 존재하고 있습니다.",
+                    "Marriages around the world recorded " +
+                    "We just know marriage for ROK" +
+                    "But in the world, many ways of marriage exsist, on their own way.",
 
-                    "유럽 국가들의 클럽클럽클럽 흔들어 " +
-                    "과연 유럽의 클럽은 어떠한 특색들을 가지고 있을까요? " +
-                    "다양한 클럽 밤문화를 한순간의 이미지로 담고자 합니다."
+                    "Feel the beeeatssss of european clubs " +
+                    "What will happen in european clubs? " +
+                    "Colorful moments of late-night lives, will be captured in images."
                 ])
                 ,contentProdCost:faker.random.number({min:100,max:300,precision:1})
                 ,contentUsingCost:faker.random.number({min:1,max:10,precision:1})
